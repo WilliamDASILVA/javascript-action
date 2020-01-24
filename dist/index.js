c0607549494eaa4479e58cad4bb75da963506665
William DA SILVA <william.da.silva@outlook.com>
2020-01-25 00:52:13 +0100

    update

--- dist/index.js
+++ dist/index.js
@@ -496,52 +496,10 @@ module.exports = require("os");
 
 const github = __webpack_require__(469);
 const core = __webpack_require__(470);
-const stats = __webpack_require__(639)
 
-const chunks = stats.chunks.map(chunk => ({
-  id: chunk.id,
-  size: chunk.size
-}))
-
-console.log('testing?', chunks)
-
-// most @actions toolkit packages have async methods
 async function run() {
   try { 
-    const prId = core.getInput('pull_request') || github.context.issue.number
-    console.log('PR ID?', prId)
-    const myToken = core.getInput('GITHUB_TOKEN');
-    // console.log('token', myToken)
-    if (prId) {
-      const octokit = new github.GitHub(myToken);
-      const content = await octokit.markdown.render({
-        data: `# This is the title
-
-          Hello world, this is the content of the comment.
-
-          > Hello ??
-        `
-      })
-      console.log('content?', content)
-      // octokit.issues.createComment({
-      //   owner: github.context.repo.owner,
-      //   repo: github.context.repo.repo,
-      //   issue_number: prId,
-      //   body: `
-      //     # This is the title
-
-      //     Hello world, this is the content of the comment.
-
-      //     > Hello ??
-      //   `
-      // })
-    }
-
-    console.log('run??')
-    chunks.forEach(chunk => {
-      console.log(`Chunk: ${chunk.id} - Size: ${chunk.size}`)
-    })
-    core.setOutput('time', new Date().toTimeString());
+    core.setOutput('pull_request', github.context.issue.number);
   } 
   catch (error) {
     core.setFailed(error.message);
@@ -8309,13 +8267,6 @@ module.exports = isPlainObject;
 /***/ (function(module) {
 
 module.exports = require("net");
-
-/***/ }),
-
-/***/ 639:
-/***/ (function(module) {
-
-module.exports = {"chunks":[{"id":"test","size":200},{"id":"hello","size":3000}]};
 
 /***/ }),
 
