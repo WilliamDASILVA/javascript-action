e806fe3a22e82421bb2e68da7329c94a57992ab2
William DA SILVA <william.da.silva@outlook.com>
2020-01-25 00:11:52 +0100

    Create main.yml (#1)
    * Create main.yml
    
    * update dist
    
    * update dist
    
    * update
    
    * update
    
    * update
    
    * update worflows
    
    * update
    
    * update
    
    * test contect with pr
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

---
+++ apps/app/vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      generateStatsFile: true,
+      openAnalyzer: true
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,17 +1,50 @@
+const github = require('@actions/github');
 const core = require('@actions/core');
-const wait = require('./wait');
+const stats = require('./stats.json')
 
+const chunks = stats.chunks.map(chunk => ({
+  id: chunk.id,
+  size: chunk.size
+}))
+
+console.log('testing?', chunks)
 
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const prId = core.getInput('pull_request') || github.context.issue.number
+    console.log('PR ID?', prId)
+    const myToken = core.getInput('GITHUB_TOKEN');
+    const octokit = new github.GitHub(myToken);
+    // // console.log('token', myToken)
+    // if (prId) {
+    //   const content = await octokit.markdown.render({
+    //     data: `# This is the title
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    //       Hello world, this is the content of the comment.
 
+    //       > Hello ??
+    //     `
+    //   })
+    //   console.log('content?', content)
+    //   // octokit.issues.createComment({
+    //   //   owner: github.context.repo.owner,
+    //   //   repo: github.context.repo.repo,
+    //   //   issue_number: prId,
+    //   //   body: `
+    //   //     # This is the title
+
+    //   //     Hello world, this is the content of the comment.
+
+    //   //     > Hello ??
+    //   //   `
+    //   // })
+    // }
+
+    console.log('run??')
+    chunks.forEach(chunk => {
+      console.log(`Chunk: ${chunk.id} - Size: ${chunk.size}`)
+    })
     core.setOutput('time', new Date().toTimeString());
   } 
   catch (error) {
